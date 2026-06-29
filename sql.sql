af81f21489998da76af0aba6810adf4bddc8a651
Ishita Tiwari <ishitatiwari060@gmail.com>
2026-06-29 10:09:09 +0530

    Patterns

--- sql.sql
+++ sql.sql
@@ -14,13 +14,44 @@ drop table students_lpu;
 -- truncate table students_lpu;
 desc students_lpu;
 alter table students_lpu change column id stu_id int;
-select * from students_lpu where batch="AI/ML";
-select * from students_lpu where batch="IT";
-select * from students_lpu where batch="ECE";
-select * from students_lpu where city="Delhi";
-select * from students_lpu where cgpa>8 and batch="IT";
-select * from students_lpu where cgpa>7.5 and batch="ECE";
-select * from students_lpu where age>20 and batch="CSE";
-select * from students_lpu where city="Delhi" or city="Mumbai";
-select * from students_lpu where batch="IT" or batch="CSE";
-select * from students_lpu where cgpa>8 and batch="ECE";
+-- select * from students_lpu where batch="AI/ML";
+-- select * from students_lpu where batch="IT";
+-- select * from students_lpu where batch="ECE";
+-- select * from students_lpu where city="Delhi";
+-- select * from students_lpu where cgpa>8 and batch="IT";
+-- select * from students_lpu where cgpa>7.5 and batch="ECE";
+-- select * from students_lpu where age>20 and batch="CSE";
+-- select * from students_lpu where city="Delhi" or city="Mumbai";
+-- select * from students_lpu where batch="IT" or batch="CSE";
+-- select * from students_lpu where cgpa>8 and batch="ECE";
+-- sql do implicit conversion
+select * from students_lpu where city!="Finland";
+select * from students_lpu where batch!="CSE" and batch!="AI/ML";
+select * from students_lpu where cgpa<=8;
+/* Advanced Filtering
+is null,between,in
+*/
+select name,age from students_lpu where age is null;
+select * from students_lpu where batch in ("AI/ML","CSE");
+select * from students_lpu where stu_id in (101,103,108);
+/*Regex
+like- to search and filter patterns
+'_' is a wildcard for exaclty 1 character 
+'%' for zero or more matching chars
+*/
+select * from students_lpu where name like "a%"; -- starting with a (case insensitive)
+select * from students_lpu where name like "%n";
+select * from students_lpu where name like "%ra%";
+select * from students_lpu where name like "_____"; -- name with only 5 characters
+-- or
+select * from students_lpu where length(name)=5; 
+--  order by
+-- select * from students_lpu order by name;
+-- select * from students_lpu order by batch,name;
+select * from students_lpu where batch="CSE";
+select * from students_lpu where age between 19 and 21;
+select * from students_lpu where age in ('CSE','IT');
+select name,age from students_lpu where name like "s%";
+select distinct batch from students_lpu;
+select name,cgpa from students_lpu;
+select * from students_lpu order by cgpa desc;
