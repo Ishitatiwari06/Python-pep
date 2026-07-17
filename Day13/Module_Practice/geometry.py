76ec803e2cbd589c54a2d79e0bc57309d1314094
Ishita Tiwari <ishitatiwari060@gmail.com>
2026-07-17 14:16:16 +0530

    Modules

---
+++ Day13/Module_Practice/geometry.py
@@ -0,0 +1,4 @@
+def area_sq(n):
+  return n*n
+def area_rec(l,b):
+  return l*b
---
+++ Day13/Module_Practice/main.py
@@ -0,0 +1,47 @@
+import caculator #user defined module
+from caculator import add,subtract
+from caculator import *
+import caculator as calc
+import geometry
+import math
+import random
+from datetime import datetime,date,time
+import statistics
+import keyword
+import os
+import sys
+
+# print(caculator.add(10,5))
+# print(caculator.subtract(10,5))
+# print(caculator.multiply(10,5))
+# print(caculator.divide(10,5))
+# print(caculator.square(9))
+
+# print(geometry.area_sq(4))
+# print(geometry.area_rec(4,6))
+
+# print(math.sqrt(25))
+# print(math.factorial(5))
+# print(math.pi)
+
+# print(random.randint(1,10))
+# print(random.choice(['Python','SQL','PowerBI']))
+
+# today=datetime.now()
+# date=date.today()
+# time=time()
+# print(today)
+# print(date)
+# print(time)
+
+# marks=[70,80,85,90,95]
+# print(statistics.mean(marks))
+# print(statistics.median(marks))
+
+# print(keyword.kwlist)
+# print(keyword.softkwlist)
+
+# print(os.getcwd())
+
+# print(sys.version)
+# print(sys.platform)
